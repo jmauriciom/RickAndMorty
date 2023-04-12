350dc50047d44633bb156dc8a84381fdfef40d4e
jmauriciom <skinet021@gmail.com>
2023-04-12 19:23:45 -0300

    quinto commit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,16 +1,13 @@
 export const ADD_FAV = 'ADD_FAV'
 export const REMOVE_FAV = 'REMOVE_FAV'
+export const FILTER_CARDS = 'FILTER_CARDS'
+export const ORDER_CARDS = 'ORDER_CARDS'
 
-export const addFav = (character) => { 
-    return {
-        type: ADD_FAV,
-        payload: character
-        }
-}
+export const addFav = (char) => ({type: ADD_FAV, payload: char})
 
-export const removefav = (id) => {
-    return {
-        type: REMOVE_FAV,
-        payload: id
-        }
-}
+export const removefav = (id) => ({type: REMOVE_FAV,payload: id})
+
+export const filterCards = (gender) => ({type: FILTER_CARDS, payload: gender})
+
+export const orderCards = (order) => ({type: ORDER_CARDS, payload: order})
+    
