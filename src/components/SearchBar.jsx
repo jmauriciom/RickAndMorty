63f1bd5f9cdd210072b60c6dced683465b50f277
jmauriciom <skinet021@gmail.com>
2023-04-13 19:05:14 -0300

    octavo commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react"
-import styles from './card.module.css'
+import React, { useState } from "react";
+import styles from './card.module.css';
 
+export default function SearchBar({ onSearch }) {
 
-export default function SearchBar({onSearch}) {
-
-  const [id, setId] = useState("")
+  const [id, setId] = useState("");
 
   const handleChange = (event) => {
-    setId(event.target.value)
+    setId(event.target.value);
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault(); 
+    }
+  }
+
+  const handleSearch = () => {
+    onSearch(id); 
+    setId("");
   }
 
   return (
-      <div>
-        <input type='search' className={styles.redondeado} value={id} onChange={handleChange} />
-        <button className={styles.boton} onClick={() =>{onSearch(id)}}>Agregar</button>
-      </div>
-    )
-  }
+    <div>
+      <input type='search' className={styles.redondeado} value={id} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <button className={styles.boton} onClick={handleSearch}>Agregar</button>
+    </div>
+  )
+}
